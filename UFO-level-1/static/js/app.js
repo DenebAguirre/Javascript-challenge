78f591464f2370bd527acad30900b538eb945c13
DenebAguirre <58702022+DenebAguirre@users.noreply.github.com>
2020-06-04 12:59:17 -0500

    finished level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,32 +8,46 @@ tableData.forEach( ufo => {
     // for each object add a row
     var row = d3.select("tbody").append("tr");
     // Get the values from the object
-    Object.values(ufo).map( x => {
+    Object.values(ufo).map( value => {
     //And print them inside a column  
-        row.append("td").text(`${x}`);
-        });
+        row.append("td").text(`${value}`);
     });
+});
 
 
-var form = d3.select("#filters")
+//var form = d3.select("#filters")
 var button = d3.select("#filter-btn")
 
-form.on("submit", playfilter)
+//form.on("submit", playfilter)
 button.on("click", playfilter)
 
+//Define handler
 function playfilter() {
     d3.event.preventDefault();
-    var inputtag = d3.select("#datetime")
-    var inputvalue = inputtag.property("value")
-    
-    filteredtable = tableData.filter( x => {
-       if (x.datetime == inputvalue) {
-            d3.select("tbody").html("");
-            d3.select("tbody").html("").append("tr").append("td").text("Si hay");
-            console.log(x);
-       } else {
-        d3.select("tbody").html("");
-        d3.select("tbody").append("tr").append("td").text("Lo siento no hay");
-   }
+    //Take the value from the input
+    var inputvalue = d3.select("#datetime").property("value");
+    //Delete the actual table
+    d3.select("tbody").html("");
+
+    //Create a new table 
+    var filteredtable = tableData.filter( ufo => {
+    //Get all the objects who match the given value
+    return ufo.datetime == inputvalue; 
     })
+    //Iterate through the objects
+    
+    .forEach( sighting => {
+        //add a row for each object
+        var row = d3.select("tbody").append("tr");
+        //Print all values adding a html column within the created row
+        Object.values(sighting).map( value => {   
+        row.append("td").text(`${value}`);
+        });
+    }); 
+
+    if (typeof filteredtable == 'undefined' || filteredtable < 0) {
+        d3.select("tbody").append("tr").append("td").text("Sorry there's no matches for provided date") ;
+    };
+       
+
 }
